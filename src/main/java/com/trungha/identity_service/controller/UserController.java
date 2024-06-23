fc2f0d93fdfc3c28eb8dc058b3ff9acf7031f321
minhtrung142003 <tieuphu142003@gmail.com>
2024-06-23 20:32:42 +0700

    ket noi database vs spring jpa and create api CRUD

---
+++ src/main/java/com/trungha/identity_service/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.trungha.identity_service.controller;
+
+import com.trungha.identity_service.dto.request.UserCreationRequest;
+import com.trungha.identity_service.dto.request.UserUpdateRequest;
+import com.trungha.identity_service.entity.User;
+import com.trungha.identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController { // tương tác với các class service
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request) {  //dùng để map data vào obj
+        return userService.createUser(request);
+    }
+    @GetMapping
+    List<User> getUsers() {
+        return userService.getUsers();
+    }
+
+    @GetMapping("/{userId}")
+    User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    User updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest request) {
+        return  userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    String User(@PathVariable String userId) {
+        userService.deleteUser(userId);
+        return "Delete has been successfull";
+    }
+
+}
---
+++ src/main/java/com/trungha/identity_service/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.trungha.identity_service.repository;
+
+import com.trungha.identity_service.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // tạo cái bean userRepository
+public interface UserRepository extends JpaRepository<User, String> {
+}
+
